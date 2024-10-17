fe7dfb290b09827143539c83d5bb3ede5a340f63
Viniarek <101534445+Viniarek@users.noreply.github.com>
2024-10-17 02:13:14 +0200

    Added more things and some of logical things.

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const computerIcon = document.getElementById("computer-icon")
+
+let humanPts = 0;
+let computerPts = 0;
+
+const humanPtsSpan = document.getElementById("human-pts");
+
+humanPtsSpan.content = humanPts;
+
+rock.addEventListener("click", (e)=>{
+    playRound(humanChoice=e.currentTarget.id);
+});
+
+paper.addEventListener("click", (e)=>{
+    playRound(humanChoice=e.currentTarget.id);
+});
+
+scissors.addEventListener("click", (e)=>{
+    playRound(humanChoice=e.currentTarget.id);
+});
+
+
+
 
 
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * (3));
-    return computerChoice == 0 ? "Rock"
-        : computerChoice == 1 ? "Paper"
-        : computerChoice == 2 ? "Scisors"
-        : "Error";
+    return computerChoice == 0 ? "rock"
+        : computerChoice == 1 ? "paper"
+        : computerChoice == 2 ? "scisors"
+        : "error";
 }
 
-function getHumanChoice(){
-    return 0;
-}
+function playRound(humanChoice){
+    const computerChoice = getComputerChoice();
+    if(computerChoice == "rock"){
+        computerIcon.innerHTML=`<i class="fa-solid fa-hand-fist"></i>`;
+    } else if(computerChoice == "paper"){
+        computerIcon.innerHTML=`<i class="fa-solid fa-hand"></i>`;
+    } else if(computerChoice == "scisors"){
+        computerIcon.innerHTML=`<i class="fa-solid fa-hand-scissors"></i>`;
+    }
+    if(humanChoice=="rock" || computerChoice == "paper"){
+        
+    }
 
-function playRound(humanChoice, computerChoice){
+    console.log(computerChoice, humanChoice);
     
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
 
 
- 
+
+
+
+
+
